dd80af9df1dd4ba8c7119931c458636cccf9fd04
Yoshiyuki Taniguchi <taniguchi@commerble.com>
2016-10-19 17:35:45 +0900

    add status check

--- example/src/activities/ProductsActivity.js
+++ example/src/activities/ProductsActivity.js
@@ -1,9 +1,11 @@
 export default function (pipeline, dispatcher) {
 
   pipeline.take('services.odata.org/V3/OData/OData.svc/Products', (context, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
-    pipeline.put('products', data.value)
+    if(response.status == 200) {
+      const data = JSON.parse(response.body)
+      console.log(data)
+      pipeline.put('products', data.value)
+    }
   })
 
   pipeline.take('products', (context, products) => {
@@ -12,5 +14,5 @@ export default function (pipeline, dispatcher) {
       products
     })
   })
-  
+
 }
