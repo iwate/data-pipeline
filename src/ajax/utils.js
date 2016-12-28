2fba5bc2a4f492af1b79e26ca8ca467bef4a4b2e
iwate <iwatesoftware@live.jp>
2016-12-28 22:56:39 +0900

    fix relative path issue

--- src/ajax/utils.js
+++ src/ajax/utils.js
@@ -1,7 +1,7 @@
 import pipeline from '../pipeline'
 
 export function putRequest(method, url, opts) {
-  const addr = new URL(url)
+  const addr = new URL(url, location)
   const uri = addr.host + addr.pathname
   pipeline.put(`requests/${uri}`, { method, url, opts })
 } 
